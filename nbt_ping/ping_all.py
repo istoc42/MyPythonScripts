f46350aa7c8db5a2d522422254cc4c7487d4fd41
Shaun Hudson <shudso91@gmail.com>
2022-04-12 10:30:51 +0100

    loop through assets and ping done

--- nbt_ping/ping_all.py
+++ nbt_ping/ping_all.py
@@ -1,8 +1,40 @@
-from w10_master_list import cell_path_master_list as ml
+import platform
+import subprocess
+from w10_master_list import asset_list as al
+
+successful = []
+
+failed = []
 
 def ping_all():
-    for dictionary in ml:
-        for val in dictionary.values():
-            print('Hello')
+    for asset in al:
+        parameter = '-n' if platform.system().lower()=='windows' else '-c'
 
-# print(ml)
+        host = asset
+
+        command = ['ping', parameter, '1', host]
+        response = subprocess.call(command)
+
+        
+
+        if response == 0:
+            print("Success")
+            successful.append(
+                {
+                    "asset_num": host,
+                }
+            )
+        else:
+            print("Failed")
+            failed.append(
+                {
+                    "asset_num": host,
+                }
+            )
+
+ping_all()
+
+print("Successful pings:")
+print(successful)
+print("\nFailed pings:")
+print(failed)
