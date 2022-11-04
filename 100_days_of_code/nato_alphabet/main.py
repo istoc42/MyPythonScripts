7e03a81a223e29a96b041cfa895c5dca4f7777d7
Shaun Hudson <shudso91@gmail.com>
2022-11-04 16:15:48 +0000

    Project complete

--- 100_days_of_code/nato_alphabet/main.py
+++ 100_days_of_code/nato_alphabet/main.py
@@ -1,32 +1,30 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+# student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
-#1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 alphabet = pandas.read_csv("nato_alphabet/nato_phonetic_alphabet.csv")
 alpha_dict = {row.letter:row.code for (index, row) in alphabet.iterrows()}
 
-
-#2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [alpha_dict[letter] for letter in word]
-print(output_list)
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [alpha_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+        break
--- 100_days_of_code/password-manager/main.py
+++ 100_days_of_code/password-manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, sample
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -30,6 +31,12 @@ def save():
     website_entry = website_input.get()
     email_entry = email_user_input.get()
     password_entry = password_input.get()
+    new_data = {
+        website_entry: {
+            "email": email_entry,
+            "password": password_entry,
+        }
+    }
     
     new_entry = f'\n{website_entry} | {email_entry} | {password_entry}'
     print(new_entry)
@@ -37,20 +44,56 @@ def save():
     if len(website_entry) == 0 or len(password_entry) == 0:
         messagebox.showerror("Oops!", "Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=f"{website_entry}", message=f"Details entered: \n\nWebsite: {website_entry}\nEmail: {email_entry}\nPassword: {password_entry}\n\nSave to Passwords.txt?")
-
-        if is_ok:
-            #Write the string to a file
-            f = open("password-manager/passwords.txt", "a")
-            f.write(new_entry)
-            f.close()
+        #Write the string to a file
+        try:
+            with open("password-manager/passwords.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+                
+        except FileNotFoundError:
+            with open("password-manager/passwords.json", "w") as data_file:
+                #Saving new data            
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
             
+            with open("password-manager/passwords.json", "w") as data_file:
+                #Update new data            
+                json.dump(data, data_file, indent=4)
+        finally:
             #Delete contents of previous entry
             website_input.delete(0,'end')
             password_input.delete(0,'end')
             
             #Place cursor back to starting position
             website_input.focus()
+            
+def find_password():
+    website = website_input.get()
+    # Check if website_entry matches an item in passwords.json
+    try:
+        with open("password-manager/passwords.json") as data_file:
+            # Read passwords.json
+            data = json.load(data_file)
+    except FileNotFoundError:
+        # Catch an exception that might occur trying to access the passwords.json showing a messagebox with the text "No Data File Found."
+        messagebox.showerror(title="Error. File Not Found", message="No Data File Found.")
+        # If yes, show a messagebox with the websites name and password
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f'Email: {email}\nPassword: {password}')
+        else:
+            # If website_entry does not exist inside the json, show a messagebox that reads "No details for the website exists."
+            messagebox.showerror(title=f"Error. No details found for {website}.", message=f"No details for {website} exists.")
+
+        
+        
+        
+    
+    
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -81,6 +124,9 @@ email_user_input.insert(0, "shudso91@gmail.com")
 
 password_input = Entry(width=32)
 password_input.grid(column=1, row=3)
+
+search_btn = Button(text="Search", width=7, command=find_password)
+search_btn.grid(column=2, row=1)
 
 generate_pass_btn = Button(text="Generate", command=password_gen)
 generate_pass_btn.grid(column=2, row=3)
